6d451c8bc85543ca8487012f2ebf0dc1203519d5
Yordan432 <dani.583@abv.bg>
2017-11-12 20:56:10 +0200

    lab javascript

--- 2.JavaScript/JavaScipt - Basics Syntax and Basics Web/largest3Numbers.js
+++ 2.JavaScript/JavaScipt - Basics Syntax and Basics Web/largest3Numbers.js
@@ -6,12 +6,4 @@ function threMaxNumbers(arr) {
        console.log(numbers[i]);
    }
 }
-function largest3Numbers(arr) {
-    let nums = arr.map(Number);
-    nums.sort((a, b) => b-a);
-    let count = Math.min(3, nums.length);
-    for(let i=0; i<count; i++) {
-        console.log(nums[i]);
-    }
-}
 largest3Numbers("10 30 15 20 50 5");
